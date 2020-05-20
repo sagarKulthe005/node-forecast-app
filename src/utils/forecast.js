234dabe97b06a35bbd37eaff5ada6fb24542d34c
Sagar Kulthe <kulthesagar005@yahoo.com>
2020-05-20 14:58:46 +0530

    text added in landing page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=92d033fdb5be166718a3539b9cbec071&query=${latitude},${longitude}`;
 
-  request({ url: url, json: true }, (err, {body}) => {
+  request({ url: url, json: true }, (err, { body } = {}) => {
     if (err) {
       callback("Unable to connect weather services!");
     } else if (body.error) {
@@ -12,7 +12,9 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `It is currently ${body.current.temperature} degress out. It feels like ${body.current.precip}% chances of rain`
+        `${body.current.weather_descriptions[0]}.
+        It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out. 
+        The humidity is ${body.current.humidity}%. The chances for rain is ${body.current.precip}% `
       );
     }
   });
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,10 +2,11 @@ const request = require("request");
 
 // Get the co-ordinates of address
 const geoCode = (address, callback) => {
-  const url =
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoic2FnYXJrdWx0aGUwMDUiLCJhIjoiY2thNHd0NTMzMDRrNzNpbnVhazBpc2MyayJ9.oO6RgrKKqz5F8x5h7lVJoQ&limit=1`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=pk.eyJ1Ijoic2FnYXJrdWx0aGUwMDUiLCJhIjoiY2thNHd0NTMzMDRrNzNpbnVhazBpc2MyayJ9.oO6RgrKKqz5F8x5h7lVJoQ&limit=1`;
 
-  request({ url, json: true }, (err, {body}) => {
+  request({ url, json: true }, (err, { body } = {}) => {
     if (err) {
       callback("Unable to connect to location services!");
     } else if (body.features.length === 0) {
